91329ec0bd9f162b0b79ae3ce7e11947fba956de
staybreeze <wunshengliao@gmail.com>
2023-12-24 12:08:48 +0800

    feat(online_store):新增後台訂單管理之刪除回當前頁面功能

--- online_store/api/del_good.php
+++ online_store/api/del_good.php
@@ -14,7 +14,7 @@ include_once "./db.php";
 
 if(isset($_GET['user'])){
     $Customer->del(['product_id' => $_GET['id'], 'customer_acc' => $_GET['user']]);
-    // header("location:../back/orders.php?do=orders");
+    header("location:../back/orders.php?do=orders");
 }else{
     $Customer->del(['product_id' => $_GET['id'], 'customer_acc' => $_SESSION['user']]);
 header("location:../cart.php");}
