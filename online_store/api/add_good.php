6b06c1952d2c655934f9755d6becd6b7b2358232
staybreeze <wunshengliao@gmail.com>
2024-01-08 15:19:55 +0800

    fix(online_store):調整增加商品的不同導引頁面

--- online_store/api/add_good.php
+++ online_store/api/add_good.php
@@ -24,8 +24,12 @@ if (isset($_SESSION['user'])) {
     echo "<pre>";
     print_r($customerResult);
     echo "</pre>";
+if(isset($_GET['cart'])){
+    header("location:../cart.php");
+}else{
 
-    header("location:../index.php#store");
+
+    header("location:../index.php#store");}
 } else {
     header("location:../login.php?error=請先登入會員");
 }
