eb6e895aba5a6e6cbcc6d8901086fb67b48c5cfb
staybreeze <wunshengliao@gmail.com>
2024-01-09 11:14:45 +0800

    feat(online_store):新增取消like_item，並連動資料庫相應資料-1

--- online_store/api/add_like.php
+++ online_store/api/add_like.php
@@ -5,27 +5,21 @@ if (!isset($_SESSION['liked_products'])) {
     $_SESSION['liked_products'] = [];
 }
 $productId = $_POST['id'];
-
+$good = $Good->find(['id' => $productId]);
+print_r($good);
 echo $productId;
 
 if (in_array($productId,$_SESSION['liked_products'])) {
   
     $index = array_search($productId, $_SESSION['liked_products']);
     unset($_SESSION['liked_products'][$index]);
+    $good['like_item']--;
 } else {
  
     $_SESSION['liked_products'][] = $productId;
+    $good['like_item'] ++;
 }
 
-
 print_r($_SESSION);
-
-
-$good = $Good->find(['id' => $productId]);
-print_r($good);
-
-$good['like_item'] += 1;
-
-
 
 $Good->save($good);
