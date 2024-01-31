35336930c26a210ba1ff2ba9c94684524d3494e3
staybreeze <wunshengliao@gmail.com>
2024-01-31 11:57:26 +0800

    fix(online_store):處理領養頁面破圖過濾之問題

--- online_store/api/file_get.php
+++ online_store/api/file_get.php
@@ -5,7 +5,11 @@
 //     return $imageInfo !== false;
 // }
 
-$location = isset($_GET['location']) ? $_GET['location'] : '';
+function isImageValid($url) {
+    $image = @imagecreatefromstring(file_get_contents($url));
+    return $image !== false;
+}
+$location =$_GET['location'];
 
 $url = 'https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL';
 $data = file_get_contents($url);
@@ -16,9 +20,10 @@ $filteredData = [];
 if ($decodedData !== null) {
     foreach ($decodedData as $item) {
         if ($item['animal_kind'] === '貓' && $item['animal_area_pkid'] == $location) {
-            // if (isImageValid($item['album_file'])) {
+            // if (isImageValid($item['album_fi+le'])) {
+                if (!empty($item['album_file'])&&isset($item['album_file'])) {
                 $filteredData[] = $item;
-            // }
+            }
         }
     }
 }
