0693518e9264939f1003adc8e5cda74a8e5d21db
staybreeze <wunshengliao@gmail.com>
2024-02-05 15:00:07 +0800

    chore(online_store):程式碼正規化

---
+++ online_store/js/search_article.js
@@ -0,0 +1,21 @@
+
+
+$(document).ready(function() {
+    $('#searchButton').click(function() {
+        var searchQuery = $('#searchInput').val();
+     
+        var encodedQuery = encodeURIComponent(searchQuery);
+        var url = "../search.php?query=" + encodedQuery;
+        window.location.href = url;
+    });
+    $('#searchInput').on('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            event.preventDefault(); 
+            var searchQuery = $('#searchInput').val();
+            var encodedQuery = encodeURIComponent(searchQuery);
+            var url = "search.php?query=" + encodedQuery;
+            window.location.href = url;
+        }
+    });
+});
+
